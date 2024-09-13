67618eb9eba6e23d8b1e5d41f24a89189c1da6de
Isa Flaherty <portia.flaherty@icloud.com>
2024-09-13 12:45:50 -0400

    add triangle movement and goofy color changing

--- assignments/assignment1_helloTriangle/main.cpp
+++ assignments/assignment1_helloTriangle/main.cpp
@@ -5,15 +5,47 @@
 #include <ew/ewMath/ewMath.h>
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
+#include <shader jail/shader.hpp>
 
 const int SCREEN_WIDTH = 1080;
 const int SCREEN_HEIGHT = 720;
 
 float vertices[] = {
-	-0.5f, -0.5f, 0.0f,
-	 0.5f, -0.5f, 0.0f,
-	 0.0f,  0.5f, 0.0f
+	// X     Y     Z     R     G     B     A
+	-0.5f, -0.5f, 0.0f, 1.0f, 1.0f, 0.0f, 1.0f,
+	 0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f,
+	 0.0f,  0.5f, 0.0f, 1.0f, 0.0f, 1.0f, 1.0f
 };
+
+const char* vertexShaderSource = R"(
+#version 330 core
+layout (location = 0) in vec3 aPos;
+layout (location = 1) in vec4 aColor;
+out vec4 Color; //Varying
+
+uniform float uTime;
+uniform float uSpeed = 3.0f;
+
+void main()
+{
+   Color = aColor; //Pass-through
+   vec3 pos = aPos;
+   pos.y += (sin(uTime * uSpeed + pos.x) * 0.25f);
+   gl_Position = vec4(pos.x, pos.y, pos.z, 1.0);
+})";
+
+
+const char* fragmentShaderSource = R"(
+#version 330 core
+out vec4 FragColor;
+in vec4 Color;
+uniform float uTime;
+
+void main()
+{
+    FragColor = Color * abs(tan(uTime));
+} 
+)";
 
 int main() {
 	printf("Initializing...");
@@ -32,6 +64,11 @@ int main() {
 		return 1;
 	}
 	//Initialization goes here!
+	//Vertex array object
+	unsigned int VAO;
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
+	
 	//create vertex buffer object
 	unsigned int VBO;
 	glGenBuffers(1, &VBO);
@@ -40,14 +77,15 @@ int main() {
 	//put data into the currently bound VBO
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+	//Position
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 7 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
 
-	const char* vertexShaderSource = "#version 330 core\n"
-    "layout (location = 0) in vec3 aPos;\n"
-    "void main()\n"
-    "{\n"
-    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-	"}\0";
+	//Color
+	glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 7 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
 
+	//shader shenanigans
 	unsigned int vertexShader;
 	vertexShader = glCreateShader(GL_VERTEX_SHADER);
 	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
@@ -61,12 +99,48 @@ int main() {
 		printf("ERROR::SHADER::VERTEX::COMPILATION_FAILED\n%s", infoLog);
 	}
 
+	unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+	if (!success) {
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+		printf("ERROR:SHADER::FRAGMENT::COMPILATION_FAILED\n%s", infoLog);
+	}
+
+	unsigned int shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success) {
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		printf("ERROR:SHADER::PROGRAM::LINKING_FAILED\n%s", infoLog);
+	}
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+
 	//Render loop
 	while (!glfwWindowShouldClose(window)) {
 		glfwPollEvents();
+		//Input
+		//Update
+		float time = (float)glfwGetTime();
+
+		//Draw
 		//Clear framebuffer
 		glClearColor(0.3f, 0.4f, 0.9f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		glUseProgram(shaderProgram);
+		glUniform1f(glGetUniformLocation(shaderProgram, "uTime"), time);
+
+		glBindVertexArray(VAO);
+
+		//Draw call
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
 		//Drawing happens here!
 		glfwSwapBuffers(window);
 	}
---
+++ core/shaderJail/shader.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "../ew/external/glad.h"
+
+namespace shaderJail {
+	class Shader {
+		Shader(const char* vertexPath, const char* fragmentPath);
+	};
+}
